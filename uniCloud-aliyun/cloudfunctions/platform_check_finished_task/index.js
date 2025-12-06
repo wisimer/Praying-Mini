2d397751437bbc8257dfff7d477799081665435c
wangxianpeng <wxp19921015@icloud.com>
2025-12-06 10:21:31 +0800

    结算逻辑

---
+++ uniCloud-aliyun/cloudfunctions/platform_check_finished_task/index.js
@@ -0,0 +1,51 @@
+'use strict';
+const db = uniCloud.database();
+exports.main = async (event, context) => {
+	// 解构传入参数
+	const { taskId, passed } = JSON.parse(event.body);
+	if (!taskId || passed === undefined) {
+		return { code: -1, msg: '缺少必要参数' };
+	}
+
+	// 查询 app-dynamic 记录
+	const dynamicRes = await db.collection('app-dynamic').doc(taskId).get();
+	if (!dynamicRes.data || dynamicRes.data.length === 0) {
+		return { code: -1, msg: '未找到对应的动态任务' };
+	}
+	const dynamic = dynamicRes.data[0];
+
+	let msgType = passed ? 4 : -4;
+	if (passed) {
+		// 执行转账逻辑（留空）
+		// TODO: 转账逻辑
+		
+		// 更新dynamicRes的status为VERIFY_PASS_WAIT_PLATFORM
+		await db.collection('app-dynamic').doc(taskId).update({
+			article_status: passed ? 5 : -5
+		});
+	}
+
+		const now = Date.now();
+		// 插入两条 app-task-message 记录
+		await db.collection('app-task-message').add([
+			{
+				msg_type: msgType,
+				state: false,
+				relevance_id: taskId,
+				from_user_id: dynamic.user_id,
+				to_user_id: dynamic.fullfill_user_id,
+				request_date: now
+			},
+			{
+				msg_type: msgType,
+				state: false,
+				relevance_id: taskId,
+				from_user_id: dynamic.fullfill_user_id,
+				to_user_id: dynamic.user_id,
+				request_date: now
+			}
+		]);
+
+	// 返回数据给客户端
+	return { code: 0, msg: '处理成功' };
+};
